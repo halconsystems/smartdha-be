86ead3a60eb238d12d999680f36f70d1ddf71fe8
jamal.uddin <jamal.uddin@dhakarachi.org>
2025-07-25 16:32:36 +0500

    Module Update added

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -26,6 +26,8 @@ else
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 //app.UseHealthChecks("/health");
