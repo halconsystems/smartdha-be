e7080ec016a36ba55884fd9015f4027b7e9c95dd
syed.shahzeb <syed.shahzeb@dhakarachi.org>
2025-08-08 15:27:36 +0500

    Changed Room.cs Entity

--- src/Domain/Entities/Room.cs
+++ src/Domain/Entities/Room.cs
@@ -22,13 +22,4 @@ public class Room : BaseAuditableEntity
 
     [MaxLength(100)]
     public string? Name { get; set; }
-
-    [Required]
-    public DateTime BookingDate { get; set; } = DateTime.UtcNow;
-
-    [Required]
-    public DateTime CheckInDate { get; set; }
-
-    [Required]
-    public DateTime CheckOutDate { get; set; }
 }
