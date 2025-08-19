14af1625818f7948cb57287335ad33fa04d3e2ad
syed.shahzeb <syed.shahzeb@dhakarachi.org>
2025-08-19 15:32:04 +0500

    Add RoomCharges WebAPI

---
+++ src/Application/Feature/Room/Commands/AddRoomCharges/Dto/RoomChargesDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DHAFacilitationAPIs.Domain.Enums;
+
+namespace DHAFacilitationAPIs.Application.Feature.RoomCharges.Dtos;
+
+public class RoomChargesDto
+{
+    public string BookingType { get; set; } = default!;
+    public decimal Charges { get; set; }
+}
