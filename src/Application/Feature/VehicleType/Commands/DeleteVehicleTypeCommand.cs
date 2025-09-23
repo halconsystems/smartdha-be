d00072afaf96e7dc8679889c88692b6c781620f7
Shahzeb Abbas <syed.shahzeb@dhakarachi.org>
2025-09-23 10:02:50 +0500

    Delete VehicleType updated

--- src/Application/Feature/VehicleType/Commands/DeleteVehicleTypeCommand.cs
+++ src/Application/Feature/VehicleType/Commands/DeleteVehicleTypeCommand.cs
@@ -9,12 +9,12 @@ using DHAFacilitationAPIs.Application.ViewModels;
 namespace DHAFacilitationAPIs.Application.Feature.BowserMake.Commands;
 public record DeleteVehicleTypeCommand(Guid Id) : IRequest<SuccessResponse<string>>;
 
-public class DeleteVehicleTypeHandler : IRequestHandler<DeleteVehicleStatusCommand, SuccessResponse<string>>
+public class DeleteVehicleTypeHandler : IRequestHandler<DeleteVehicleTypeCommand, SuccessResponse<string>>
 {
     private readonly IOLHApplicationDbContext _context;
     public DeleteVehicleTypeHandler(IOLHApplicationDbContext context) => _context = context;
 
-    public async Task<SuccessResponse<string>> Handle(DeleteVehicleStatusCommand request, CancellationToken cancellationToken)
+    public async Task<SuccessResponse<string>> Handle(DeleteVehicleTypeCommand request, CancellationToken cancellationToken)
     {
         var entity = await _context.VehicleTypes
             .FirstOrDefaultAsync(x => x.Id == request.Id && x.IsDeleted != true, cancellationToken);
