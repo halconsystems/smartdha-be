82d6c0a191872e5b266ef5e9bb50674f0a19dbd0
Shahzeb Abbas <syed.shahzeb@dhakarachi.org>
2025-09-23 16:56:47 +0500

    Bowser Phase Capacity + Driver Assignment + Available Drivers API

---
+++ src/Web/Controller/BowserAssignmentController.cs
@@ -0,0 +1,36 @@
+﻿using DHAFacilitationAPIs.Application.Feature.BowserAssignment.Commands.AssignDriverToBowser;
+using DHAFacilitationAPIs.Application.Feature.BowserAssignment.Queries.GetAvailableDriverVehicles;
+using DHAFacilitationAPIs.Application.Feature.BowserCapacities.Commands;
+using DHAFacilitationAPIs.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DHAFacilitationAPIs.Web.Controller;
+[Route("api/[controller]")]
+[ApiController]
+public class BowserAssignmentController : BaseApiController
+{
+    private readonly IMediator _mediator;
+
+    public BowserAssignmentController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("Get-Available Driver Vehicles"), AllowAnonymous]
+    public async Task<ActionResult<List<AvailableDriverVehicleDto>>> GetAvailableDriverVehicles(
+        [FromQuery] DateTime requestedDeliveryDate,
+        [FromQuery] Guid phaseId,
+        [FromQuery] Guid bowserCapacityId)
+    {
+        var result = await _mediator.Send(new GetAvailableDriverVehiclesQuery(requestedDeliveryDate, phaseId, bowserCapacityId));
+        return Ok(result);
+    }
+
+    [HttpPost("assign-driver"), AllowAnonymous]
+    public async Task<ActionResult<SuccessResponse<Guid>>> AssignDriver([FromBody] AssignDriverToBowserDto dto)
+    {
+        var response = await _mediator.Send(new AssignDriverToBowserCommand(dto));
+        return Ok(response);
+    }
+}
