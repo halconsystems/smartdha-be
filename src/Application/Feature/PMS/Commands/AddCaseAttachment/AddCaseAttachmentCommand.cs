51cd3676fc23bb0509fce00334d70ba00cbf1353
jamal.uddin <jamal.uddin@dhakarachi.org>
2026-01-13 17:17:01 +0500

    PMS Changes

---
+++ src/Application/Feature/PMS/Commands/AddCaseAttachment/AddCaseAttachmentCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DHAFacilitationAPIs.Application.Common.Interfaces;
+using DHAFacilitationAPIs.Application.ViewModels;
+
+namespace DHAFacilitationAPIs.Application.Feature.PMS.Commands.AddCaseAttachment;
+public record AddCaseAttachmentCommand(
+    Guid UserId,
+    int CaseId,
+    int PrerequisitesID,
+    string FilePath  //  saved file path
+) : IRequest<SuccessResponse<string>>;
+public class AddCaseAttachmentCommandHandler
+    : IRequestHandler<AddCaseAttachmentCommand, SuccessResponse<string>>
+{
+    private readonly IProcedureService _sp;
+
+    public AddCaseAttachmentCommandHandler(IProcedureService sp)
+    {
+        _sp = sp;
+    }
+
+    public async Task<SuccessResponse<string>> Handle(
+        AddCaseAttachmentCommand request,
+        CancellationToken cancellationToken)
+    {
+        var parameters = new DynamicParameters();
+        parameters.Add("@userID", request.UserId, DbType.Guid);
+        parameters.Add("@caseID", request.CaseId, DbType.Int32);
+        parameters.Add("@PrerequisitesID", request.PrerequisitesID, DbType.Int32);
+        parameters.Add("@FilePath", request.FilePath, DbType.String);
+        parameters.Add("@msg", dbType: DbType.String, size: 500, direction: ParameterDirection.Output);
+
+        await _sp.ExecuteAsync(
+            "USP_AddCaseAttachments",
+            parameters,
+            cancellationToken,
+            "PMSConnection"
+        );
+
+        var message = parameters.Get<string>("@msg") ?? "Attachment added";
+
+        return new SuccessResponse<string>(message);
+    }
+}
+
---
+++ src/Application/Feature/PMS/Queries/GetProcesses/GetProcessTypesQuery.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DHAFacilitationAPIs.Application.Common.Interfaces;
+using DHAFacilitationAPIs.Application.ViewModels;
+using DHAFacilitationAPIs.Domain.Enums;
+
+namespace DHAFacilitationAPIs.Application.Feature.PMS.Queries.GetProcesses;
+public record GetProcessTypesQuery(
+    int UserId,
+    AppType AppID
+) : IRequest<SuccessResponse<List<ProcessTypeDto>>>;
+
+public class GetProcessTypesQueryHandler
+    : IRequestHandler<GetProcessTypesQuery, SuccessResponse<List<ProcessTypeDto>>>
+{
+    private readonly IProcedureService _sp;
+
+    public GetProcessTypesQueryHandler(IProcedureService sp)
+    {
+        _sp = sp;
+    }
+
+    public async Task<SuccessResponse<List<ProcessTypeDto>>> Handle(
+        GetProcessTypesQuery request,
+        CancellationToken cancellationToken)
+    {
+        var parameters = new DynamicParameters();
+
+        parameters.Add("@user_ID", request.UserId, DbType.Int32);
+        parameters.Add("@AppID", request.AppID, DbType.Int32);
+        parameters.Add("@msg", dbType: DbType.String, size: 150, direction: ParameterDirection.Output);
+
+        var (outParams, data) = await _sp.ExecuteWithListAsync<ProcessTypeDto>(
+           "USP_SelectProcess",
+           parameters,
+           cancellationToken,
+           "PMSConnection");
+
+        string message = outParams.Get<string>("@msg") ?? "Success";
+
+        return new SuccessResponse<List<ProcessTypeDto>>(data, message);
+    }
+}
