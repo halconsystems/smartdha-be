9ed7331d678dd6d566cb8051883f670392a066bf
DHA Admin <DHAAdmin@gmail.com>
2026-02-01 03:11:44 +0500

    done

--- src/Infrastructure/Migrations/CBMSApplicationDb/20260131221103_Newtabkecreatedforimages.Designer.cs
+++ src/Infrastructure/Migrations/CBMSApplicationDb/20260131221103_Newtabkecreatedforimages.Designer.cs
@@ -4,6 +4,7 @@ using DHAFacilitationAPIs.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DHAFacilitationAPIs.Infrastructure.Migrations.CBMSApplicationDb
 {
     [DbContext(typeof(CBMSApplicationDbContext))]
-    partial class CBMSApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260131221103_Newtabkecreatedforimages")]
+    partial class Newtabkecreatedforimages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
